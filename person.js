44cb94a7ce9748b3e774f68a6ba588db54b55ed6
Tobiloba <tabilon58@gmail.com>
2026-02-16 10:34:02 +0100

    my classwork

---
+++ person.js
@@ -0,0 +1,20 @@
+//add a new property email to the person object
+//update the age property of the person 
+//delete the last name property from person 
+//acces and print the value of first firstname using dot notation and bracket notation
+
+const person = {
+    firstname : "Tobiloba",
+    lastname : "Abidemi",
+    age : 6,
+    grade : 54
+}
+
+person.email = "tabilon58@gmail.com";
+person.age = 5;
+
+delete person.lastname
+
+
+
+console.log(person)
---
+++ student.js
@@ -0,0 +1,8 @@
+create an object student with properties name ae grade
+Add a new property school to the student object
+change the value of grade to a new value
+remove the age property from the object
+print all the keys of the object
+print all the values of the object
+
+
